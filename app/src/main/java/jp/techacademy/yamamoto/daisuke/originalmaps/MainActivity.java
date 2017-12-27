babc221bb9c10764ebdad16ec4abb56b22101ca8
d_yamamoto <nice_day81@yahoo.co.jp>
2017-12-27 11:54:12 +0900

    地図上の現在地ボタンを右下に移動できない

--- app/src/main/java/jp/techacademy/yamamoto/daisuke/originalmaps/MainActivity.java
+++ app/src/main/java/jp/techacademy/yamamoto/daisuke/originalmaps/MainActivity.java
@@ -1,19 +1,23 @@
 package jp.techacademy.yamamoto.daisuke.originalmaps;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -23,10 +27,11 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    // 六甲山：北緯34度46分41秒, 東経135度15分49秒}
-    private double mLatitude = 34.0d + 46.0d/60 + 41.0d/(60*60);
-    private double mLongitude = 135.0d + 15.0d/60 + 49.0d/(60*60);
-    private GoogleMap 
+    // 高尾山
+    private double mLatitude = 35.0d + 37.0d / 60 + 56.9d / (60 * 60);
+    private double mLongitude = 139.0d + 16.0d / 60 + 11.7d / (60 * 60);
+
+    private View mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +48,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             startActivity(intent);
         }
 
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapView = mapFragment.getView();
+        mapFragment.getMapAsync(this);
+
 
         //Topのフローティングボタンが押された時
-        FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -81,17 +91,48 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+            mMap = googleMap;
+            //現在地ボタン
+            mMap.setMyLocationEnabled(true);
+
+            if (mapView != null &&
+                    mapView.findViewById(Integer.parseInt("1")) != null) {
+                // Get the button view
+                View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+                // and next place it, on bottom right (as Google Maps app)
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                        locationButton.getLayoutParams();
+                // position on right bottom
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+                layoutParams.setMargins(0, 0, 30, 30);
+            }
 
 
-
-        //現在地ボタン
-        mMap.setMyLocationEnabled(true);
         //コンパス
         mMap.getUiSettings().setCompassEnabled(true);
         //ズームインアウトボタン
-        mMap.getUiSettings().setZoomControlsEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+
+        //LocationManagerの取得
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        //GPSから現在地の情報を取得
+        Location myLocate = locationManager.getLastKnownLocation("gps");
+        if (myLocate != null) {
+            //現在地情報取得成功
+            //緯度の取得
+            int latitude = (int) (myLocate.getLatitude() * 1e6);
+            //経度の取得
+            int longitude = (int) (myLocate.getLongitude() * 1e6);
+            //GeoPointに緯度・経度を指定
+        } else {
+            //現在地情報取得失敗時の処理
+            Toast.makeText(this, "現在地取得できませんでした！", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
 }
+
 
